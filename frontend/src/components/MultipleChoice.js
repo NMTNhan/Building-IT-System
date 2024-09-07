9e066e7c0c754de3d3b50b3844cf725c8aad7c08
Terry275 <116662781+Terry275@users.noreply.github.com>
2024-09-08 01:43:33 +0700

    Update frontend

--- frontend/src/components/MultipleChoice.js
+++ frontend/src/components/MultipleChoice.js
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MultipleChoice = ({ question, onAnswer }) => {
+
+
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [feedback, setFeedback] = useState(null);
+
+
   if (!question || !question.options) {
     return <p>Loading...</p>;
   }
 
+
   const handleAnswer = (selectedOption) => {
-    onAnswer(selectedOption === question.answer);
+    setSelectedOption(selectedOption);
+    const isCorrect = selectedOption === question.answer;
+    setFeedback(isCorrect ? 'correct' : 'incorrect');
+
+    setTimeout(() => {
+      onAnswer(isCorrect);
+      setSelectedOption(null);
+      setFeedback(null);
+    }, 2000);
   };
 
   return (
-    <div>
-      <h2 className="text-2xl font-bold mb-4">{question.question}</h2>
-      <ul className="space-y-2">
-        {question.options.map((option) => (
-          <li key={option}>
+    <div className="w-full">
+      <div className="border-4 bg-white border-gray-400 p-12 rounded-lg shadow-xl flex items-center justify-center min-h-[100px] mb-3">
+        <h2 className="text-3xl font-bold">{question.question}</h2>
+      </div>
+
+      {/* Answer Options */}
+      <div className="grid grid-cols-2 gap-8 mt-16">
+        {question.options.map((option) => {
+          let buttonClass = "bg-white border-2 border-gray-300 hover:bg-orange-300 text-black py-5 px-6 md:text-lg rounded-md shadow-md transition-colors duration-300 w-full";
+
+          if (feedback) {
+            if (option === question.answer) {
+              buttonClass = feedback === 'correct' && selectedOption === option
+                ? 'bg-green-400 text-white rounded-md shadow-md' 
+                : 'bg-green-400 text-white rounded-md shadow-md';
+            } else if (selectedOption === option) {
+              buttonClass = 'bg-red-400 text-white rounded-md shadow-md';
+            }
+          }
+
+          return (
             <button
-              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+              key={option}
+              className={buttonClass}
               onClick={() => handleAnswer(option)}
+              disabled={!!feedback} 
             >
               {option}
             </button>
-          </li>
-        ))}
-      </ul>
+          );
+        })}
+      </div>
     </div>
   );
 };
